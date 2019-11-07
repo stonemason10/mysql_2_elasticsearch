96d4c08e60d3120f741c4c1e6874ff48a2ba19d3
stone <1165860412@qq.com>
2019-11-07 17:20:41 +0800

    去掉修改表ID,去掉空字符串和制表符

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
-var elasticsearch = require('elasticsearch');
-var mysql = require('mysql');
+const elasticsearch = require('elasticsearch');
+const mysql = require('mysql');
 
-var es_jdbc = require('./lib/pull-push.js');
-var pullDataFromMysql = es_jdbc.pull;
-var pushDataToElastic = es_jdbc.push;
+const es_jdbc = require('./lib/pull-push.js');
+let pullDataFromMysql = es_jdbc.pull;
+let pushDataToElastic = es_jdbc.push;
 
-var transSingleTable = function(pool, client, config, $table, sqlPhrase, $type, filter_map, exception_handler, callback) {
+let transSingleTable = function(pool, client, config, $table, sqlPhrase, $type, filter_map, exception_handler, callback) {
   // 配置项
-  var es_config = {
+  let es_config = {
     host: config.elasticsearch.host_config,
     chunkSize: config.elasticsearch.chunkSize,
     timeout: config.elasticsearch.timeout,
@@ -37,37 +37,37 @@ var transSingleTable = function(pool, client, config, $table, sqlPhrase, $type, 
 
 module.exports = function(config, callback) {
   // 开始建立 MySQL 数据库连接
-  var pool = mysql.createPool( config.mysql );
+  let pool = mysql.createPool( config.mysql );
   console.log('开始连接数据库：' + config.mysql.host + ':' + config.mysql.port + '/' + config.mysql.database);
 
   // 开始建立 elasticsearch 客户端服务
-  var client = new elasticsearch.Client(config.elasticsearch.host_config);
+  let client = new elasticsearch.Client(config.elasticsearch.host_config);
   console.log('开始建立 elasticsearch 客户端连接...');
 
-  var mapLen = JSON.stringify(config.riverMap).match(/\=\>/g).length;
-  var successArr = [];
-  var failedArr = [];
+  let mapLen = JSON.stringify(config.riverMap).match(/\=\>/g).length;
+  let successArr = [];
+  let failedArr = [];
 
-  for (var key in config.riverMap) {
-    var curTable = key.split('=>')[0].replace(/\s+/g, '');
-    var curType = key.split('=>')[1].replace(/\s+/g, '');
+  for (let key in config.riverMap) {
+    let curTable = key.split('=>')[0].replace(/\s+/g, '');
+    let curType = key.split('=>')[1].replace(/\s+/g, '');
 
     // filter_out
     if (!config.riverMap[key].filter_out || config.riverMap[key].filter_out.length == 0) {
-      var filter_map = [];
+      let filter_map = [];
     } else {
-      var filter_map = config.riverMap[key].filter_out;
+      let filter_map = config.riverMap[key].filter_out;
     }
 
     // exception_handler
     if (!config.riverMap[key].exception_handler) {
-      var exception_handler = {};
+      let exception_handler = {};
     } else {
-      var exception_handler = config.riverMap[key].exception_handler;
+      let exception_handler = config.riverMap[key].exception_handler;
     }
 
     // 表名为SQL，则表示将SQL查询结果存入对应type
-    var sqlPhrase = !config.riverMap[key].SQL ? '' : config.riverMap[key].SQL;
+    let sqlPhrase = !config.riverMap[key].SQL ? '' : config.riverMap[key].SQL;
 
     transSingleTable(pool, client, config, curTable, sqlPhrase, curType, filter_map, exception_handler, function(state) {
       if (state) {
